1e2416ba9b70019a6d01b417cd7b806c99f5fa74
romaniudin <romanjudin87@gmail.com>
2019-01-23 22:24:15 -0800

    Adjusted toJSX components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
 
-import Nav from './Nav';
-import Home from './Home';
-import NewScenario from './NewScenario';
-import NewExpense from './NewExpense';
-import NewIncome from './NewIncome';
-import Scenario from './Scenario';
-import ScenarioDetails from './ScenarioDetails';
-import ScenarioList from './ScenarioList';
-import Profile from './Profile';
-import Footer from './Footer';
-import DecisionList from './DecisionList';
+import Nav from './components/Nav.jsx';
+import Home from './components/Home.jsx';
+import NewScenario from './components/NewScenario.jsx';
+import NewExpense from './components/NewExpense.jsx';
+import NewIncome from './components/NewIncome.jsx';
+import Scenario from './components/Scenario.jsx';
+import ScenarioDetails from './components/ScenarioDetails.jsx';
+import ScenarioList from './components/ScenarioList.jsx';
+import Profile from './components/Profile.jsx';
+import Footer from './components/Footer.jsx';
+import DecisionList from './components/DecisionList.jsx';
 
 class App extends Component {
 
